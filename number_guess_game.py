4046ffddb5791ea40368867c249417e46c00d249
framesandpackets <michaelhanna272@hotmail.co.uk>
2022-08-24 21:41:38 +0100

    difficulty_check function created & code cleaned up

--- number_guess_game.py
+++ number_guess_game.py
@@ -1,81 +1,71 @@
 import random
 
-# def easy_mode():
-#     num = random.randrange(1,100)
-#     attempts = 10
-#     while attempts != 0:
-#         print(num)
-#         print(f"You have {attempts} attempts remaining to guess the number correctly")
-#         guess = int(input("Make a guess: "))
-#         if guess < num:
-#             print("Your guess was to low")
-#             attempts -= 1
-#         elif guess > num:
-#             print("Your guess was to high")
-#             attempts -= 1
-#         elif guess == num:
-#             print("Well done! You guessed correctly, now try 'hard' mode!")
-#             exit()
-#         else:
-#             print("Your guess was not a number! Try again!")
+#ASCII ART GENERATED AT: https://fsymbols.com/generators/carty/
+
+banner = """ 
+
+░█████╗░██╗░░░░░██╗    ███╗░░██╗██╗░░░██╗███╗░░░███╗██████╗░███████╗██████╗░
+██╔══██╗██║░░░░░██║    ████╗░██║██║░░░██║████╗░████║██╔══██╗██╔════╝██╔══██╗
+██║░░╚═╝██║░░░░░██║    ██╔██╗██║██║░░░██║██╔████╔██║██████╦╝█████╗░░██████╔╝
+██║░░██╗██║░░░░░██║    ██║╚████║██║░░░██║██║╚██╔╝██║██╔══██╗██╔══╝░░██╔══██╗
+╚█████╔╝███████╗██║    ██║░╚███║╚██████╔╝██║░╚═╝░██║██████╦╝███████╗██║░░██║
+░╚════╝░╚══════╝╚═╝    ╚═╝░░╚══╝░╚═════╝░╚═╝░░░░░╚═╝╚═════╝░╚══════╝╚═╝░░╚═╝
+
+░██████╗░██╗░░░██╗███████╗░██████╗░██████╗  ░██████╗░░█████╗░███╗░░░███╗███████╗██╗
+██╔════╝░██║░░░██║██╔════╝██╔════╝██╔════╝  ██╔════╝░██╔══██╗████╗░████║██╔════╝██║
+██║░░██╗░██║░░░██║█████╗░░╚█████╗░╚█████╗░  ██║░░██╗░███████║██╔████╔██║█████╗░░██║
+██║░░╚██╗██║░░░██║██╔══╝░░░╚═══██╗░╚═══██╗  ██║░░╚██╗██╔══██║██║╚██╔╝██║██╔══╝░░╚═╝
+╚██████╔╝╚██████╔╝███████╗██████╔╝██████╔╝  ╚██████╔╝██║░░██║██║░╚═╝░██║███████╗██╗
+░╚═════╝░░╚═════╝░╚══════╝╚═════╝░╚═════╝░  ░╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚══════╝╚═╝
+
+"""
+
+#CHANCES PLATER GETS IN EASY MODE:
+EASY_ATTEMPTS = 10
+
+#CHANCES PLATER GETS IN EASY MODE:
+HARD_ATTEMPTS = 5
+
+#FUNCTION THAT CHECKS HOW MANY ATTEMPTS TO ASSIGN TO THE USER
+def difficulty_check():
+    difficulty = input("\nPlease choose difficulty! Type 'easy' or 'hard': ")
+    if difficulty.lower() == "easy":
+        return EASY_ATTEMPTS
+    elif difficulty.lower() == "hard":
+        return HARD_ATTEMPTS
 
 
-# def hard_mode():
-#     num = random.randrange(1,100)
-#     attempts = 5
-#     while attempts != 0:
-#         print(num)
-#         print(f"You have {attempts} attempts remaining to guess the number correctly")
-#         guess = int(input("Make a guess: "))
-#         if guess < num:
-#             print("Your guess was to low")
-#             attempts -= 1
-#         elif guess > num:
-#             print("Your guess was to high")
-#             attempts -= 1
-#         elif guess == num:
-#             print("Well done! You guessed correctly! Ultra Extreme Hard Mode coming soon!"")
-#             exit()
-#         else:
-#             print("Your guess was not a number! Try again!")
+#MAIN GAME FUNCTION
+def number_game():
+
+    print(banner)
+
+    print("Hello, I'm thinking of a number between 1 - 100!")
+
+    #ASSIGNING ATTEMPTS TO USER
+    attempts = difficulty_check()
+
+    #GENERATING NUMBER BETWEEN 1- 100
+    num = random.randrange(1,100)
 
 
+    #### THIS NEEDS A LITTLE MORE WORK AND CLEAN UP! ####
+    while attempts != 0:
+        #DEBUG: LETS YOU SEE THE RANDOMLY GENERATED NUMBER
+        # print(num)    
+        print(f"You have {attempts} attempts remaining to guess the number correctly")
+        guess = int(input("Make a guess: "))
+        if guess < num:
+            print("Your guess was to low")
+            attempts -= 1
+        elif guess > num:
+            print("Your guess was to high")
+            attempts -= 1
+        elif guess == num:
+            print(f"Well done! The number was {num}, You guessed correctly!")
+            exit()
+        else:
+            print("Your guess was not a number! Try again!")
 
-print("Welcome to The Number Guess Game!")
+number_game()
 
-print("I am currently thinking of a number between 1 - 100")
-
-difficulty = input("Please choose difficulty! Type 'easy' or 'hard': ")
-
-if difficulty == "easy":
-    attempts = 10
-elif difficulty == "hard":
-    attempts = 5
-
-num = random.randrange(1,100)
-
-while attempts != 0:
-    print(num)
-    print(f"You have {attempts} attempts remaining to guess the number correctly")
-    guess = int(input("Make a guess: "))
-    if guess < num:
-        print("Your guess was to low")
-        attempts -= 1
-    elif guess > num:
-        print("Your guess was to high")
-        attempts -= 1
-    elif guess == num:
-        print("Well done! You guessed correctly!")
-        exit()
-    else:
-        print("Your guess was not a number! Try again!")
-
-
-
-
-
-
-# if difficulty.lower() == "easy":
-#     easy_mode()
-# elif difficulty.lower() == "hard":
-#     hard_mode()
